56d0da758e6fca188bda1df92a390504a9e00456
shubhamkamble <kshub44kamble@gmail.com>
2020-04-23 08:28:27 +0530

    added passwor validation

---
+++ day14/passwordvalid.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+shopt -$ extglob
+echo "Enter Password"
+read pass
+pat="^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$"
+if [[ $pass =~ $pat ]]
+then
+	echo valid;
+else
+	echo Invalid;
+fi
